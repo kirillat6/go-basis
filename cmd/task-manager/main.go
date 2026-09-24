054bc88f0a2f8b31a7da2a95e2bec3e98f72358b
Kirill Tihonov <taylormr047@gmail.com>
2026-09-24 20:50:32 +0300

    test

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -1,5 +1,95 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/kirillat6/go-basis/internal/task"
+)
+
+
+func main() {
+	tasks := task.TaskManager{}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Выберите действие:")
+		fmt.Println("1. Добавить Задачу")
+		fmt.Println("2. Удалить Задачу")
+		fmt.Println("3. Выполнить Задачу")
+		fmt.Println("4. Просмотреть все задачи")
+		fmt.Print("Введите ваш вариант: ")
+		num, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Такого варианта нет!")
+			continue
+		}
+
+		num = strings.TrimSpace(num)
+
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			fmt.Println("Введите число")
+			continue
+		}
+
+		if n == 1 {
+			fmt.Print("Введите название задачи: ")
+			title, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Считывание ответа прошло не успешно")
+				continue
+			}
+			title = strings.TrimSpace(title)
+			tasks.CreateTask(title)
+		}
+		if n == 2 {
+			fmt.Print("Введите номер задачи: ")
+			strId, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Считывание ответа прошло не успешно")
+				continue
+			}
+			strId = strings.TrimSpace(strId)
+			id, err := strconv.Atoi(strId)
+			if err != nil {
+				fmt.Println("Преобразование id прошло не успешно")
+				continue
+			}
+			err = tasks.DeleteTask(id)
+			if err != nil {
+				fmt.Println("Задача не найдена!")
+				continue
+			}
+		}
+		if n == 3 {
+			fmt.Print("Введите номер задачи: ")
+			strId, err := reader.ReadString('\n')
+
+			if err != nil {
+				fmt.Println("Считывание ответа прошло не успешно")
+				continue
+			}
+			strId = strings.TrimSpace(strId)
+
+			id, err := strconv.Atoi(strId)
+			if err != nil {
+				fmt.Println("Преобразование id прошло не успешно")
+				continue
+			}
+			err = tasks.CompleteTask(id)
+			if err != nil {
+				fmt.Println("Задача не найдена!")
+				continue
+			}
+		}
+		if n == 4 {
+			allTasks := tasks.GetTasks()
+			for _,task := range allTasks {
+				fmt.Println(task)
+			}
+		}
+	}
 }
--- internal/task/task.go
+++ internal/task/task.go
@@ -1,1 +1,49 @@
 package task
+
+import "fmt"
+
+type Task struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+type TaskManager struct {
+	tasks []Task
+	nextID int 
+}
+
+
+func (h *TaskManager) CreateTask(title string) {
+	h.nextID++
+	newTask := Task{
+		ID:        h.nextID,
+		Title:     title,
+		Completed: false,
+	}
+	h.tasks = append(h.tasks, newTask)
+}
+
+func (h *TaskManager) DeleteTask(id int) error {
+		for i := range h.tasks {
+		if h.tasks[i].ID == id {
+			h.tasks = append(h.tasks[:i], h.tasks[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Задача не найдена!")
+}
+
+func (h *TaskManager) CompleteTask(id int) error{
+	for i := range h.tasks {
+		if h.tasks[i].ID == id {
+			h.tasks[i].Completed = true
+			return nil
+		}
+	}
+	return fmt.Errorf("Задача не найдена!")
+}
+
+func (h *TaskManager) GetTasks() []Task {
+	return h.tasks
+}
